ea98b410c20e8db45b9aa246d7108710ff706252
chanchana <north1602@gmail.com>
2021-11-21 22:13:36 +0700

    Rename package name

--- frontity.settings.js
+++ frontity.settings.js
@@ -9,7 +9,7 @@ const settings = {
   },
   "packages": [
     {
-      "name": "@frontity/mars-theme",
+      "name": "@frontity/pordee-theme",
       "state": {
         "theme": {
           "menu": [
