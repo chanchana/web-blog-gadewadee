95d7e559958763d51ce1cc6bfaca188820d083b5
chanchana <north1602@gmail.com>
2021-11-22 22:20:58 +0700

    Fix related posts

--- packages/pordee-theme/src/components/utils/RelatedPost.js
+++ packages/pordee-theme/src/components/utils/RelatedPost.js
@@ -6,6 +6,9 @@ export const getRelatedPosts = async (state, actions, post, count = 3) => {
     await actions.source.fetch(url);
     const posts = state.source.get(url).items.map(({ type, id }) => state.source[type][id]);
     console.log(posts);
-    const relatedPosts = posts.filter(p => p.categories.some(c => post.categories.includes(c))).slice(0, count);
+    const relatedPosts = posts
+        .filter(p => p.id !== post.id)    
+        .filter(p => p.categories.some(c => post.categories.includes(c)))
+        .slice(0, count);
     return relatedPosts;
 }
