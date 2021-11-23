8835b836131a6814449b9019f2b40adbb1748a4a
chanchana <north1602@gmail.com>
2021-11-23 11:16:02 +0700

    Add default posts for related posts

--- packages/pordee-theme/src/components/utils/RelatedPost.js
+++ packages/pordee-theme/src/components/utils/RelatedPost.js
@@ -5,10 +5,15 @@ export const getRelatedPosts = async (state, actions, post, count = 3) => {
     const url = `/`;
     await actions.source.fetch(url);
     const posts = state.source.get(url).items.map(({ type, id }) => state.source[type][id]);
-    console.log(posts);
-    const relatedPosts = posts
-        .filter(p => p.id !== post.id)    
+    const otherPosts = posts.filter(p => p.id !== post.id);
+    const relatedPosts = otherPosts
         .filter(p => p.categories.some(c => post.categories.includes(c)))
         .slice(0, count);
+    if (relatedPosts.length < count) {
+        const fillPosts = otherPosts
+            .filter(p => !relatedPosts.map(rp => rp.id).includes(p.id))
+            .slice(0, count - relatedPosts.length);
+        return relatedPosts.concat(fillPosts);
+    }
     return relatedPosts;
 }
